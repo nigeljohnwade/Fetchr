e82ac69dd618344eb8b38ab3fa26c72347542c7b
Nigel Wade <nigeljohnwade@gmail.com>
2016-02-11 17:24:35 +0000

    Add search button control

--- js/main.js
+++ js/main.js
@@ -1,28 +1,34 @@
 var config = require("./../config.js");
 var Flickr = require("flickrapi");
 
-Flickr.tokenOnly(config.flickrOptions, function(error, flickr) {
-	if(error){
-		console.log(error);
-	}
-	flickr.photos.search({
-	  user_id: '19471305@N00',
-	  page: 1,
-	  per_page: 5
-	}, function(err, result) {
-		if(err){
-			console.log('Error');
-			console.log(err)
-		}else{
-			console.log(result);
-			document.write(result.photos.total);
-			document.write('\n');
-			for(var i = 0 ; i < result.photos.photo.length ; i++){
-				document.write(result.photos.photo[i].title);
-				document.write('\n');
-			}
-		}
-	});
-});
+function searchFlickr(){
+    console.log('searching...');
+    Flickr.tokenOnly(config.flickrOptions, function(error, flickr) {
+        if(error){
+            console.log(error);
+        }
+        flickr.photos.search({
+          user_id: '19471305@N00'
+        }, function(err, result) {
+            if(err){
+                console.log('Error');
+                console.log(err)
+            }else{
+                console.log(result);
+                for(var i = 0 ; i < result.photos.photo.length ; i++){
+                    currentImg = result.photos.photo[i];
+                    var imgTarget = document.getElementById('picture-panel');
+                    tempImg = document.createElement('img');
+                    tempImg.setAttribute('src', 'https://farm' + currentImg.farm + '.staticflickr.com/' + currentImg.server + '/' + currentImg.id + '_' + currentImg.secret + '_t.jpg');
+                    tempImg.setAttribute('title', result.photos.photo[i].title);
+                    imgTarget.appendChild(tempImg);
+                }
+            }
+        });
+    });
+}
 
+document.addEventListener('click', function(){
+    searchFlickr();
+});
 console.log('End');
